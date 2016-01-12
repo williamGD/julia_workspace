2a1beb3c386e5fa2fdb217f586d296ca8cb5d24c
william.W <1207819230@qq.com>
2016-01-13 00:46:48 +0800

    2016_1_13

--- ADforward.jl
+++ ADforward.jl
@@ -1,17 +1,27 @@
+###########################################
+#  This mudule is for Algorithmic differentiation
+#  Author: wangjiechao
+#  Date: 2016.1.13
+#  Version: 1.0
+
+
 import  Base.sin
 import  Base.cos
 import  Base
+
 #define a type which stored primitive function and derived function
 type Data
     value::Vector{Any}
     derivate::Vector{Any}
 end
+
 function ADforward(f::Function)
     g(x)=f(Data(x,1).derivate;#a0=x,b0=1
     ##g(x)=f(Data(x,Int64(ones(length(x))))).derivate;#a0=x,b0=1
 end
+
 ##operator overloading
-##operator *
+##operator 
 function *(T1::Data,T2::Data)
   return Data(T1.value.*T2.value,T1.value.*T2.derivate+T1.derivate.*T2.value)
 end
